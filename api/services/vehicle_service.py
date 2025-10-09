fe43ba0f1151a2696dd2c0f90eef88403fdb835c
WarMartijn <1058740@hr.nl>
2025-10-09 16:51:28 +0200

    Finished up the remaining vehicle crud methods

--- api/services/vehicle_service.py
+++ api/services/vehicle_service.py
@@ -1,15 +1,16 @@
 from fastapi import HTTPException, status
 import json
+from datetime import datetime
 from api.models.user_models import User
 from api.storage_utils import load_json
 from validation_service import ValidationService
-from storage_utils import load_vehicle_data
+from storage_utils import load_vehicle_data,save_data
 from user_service import UserService
 class VehicleService:
     @staticmethod
     def checkForVehicle(session_user : User , Vid : str):
         """Check if the vehicle exists"""
-        vehicles = load_vehicle_data
+        vehicles = load_vehicle_data()
         uvehicles = vehicles.get(session_user["username"], {})
         if not Vid in uvehicles:
               raise HTTPException(
@@ -17,29 +18,122 @@ class VehicleService:
                 detail="Not Found"
             )
         
+    @staticmethod
+    def check_for_parameters(data):
+        for field in ["name", "license_plate"]:
+            if not field in data:
+                raise HTTPException(
+                status_code=status.HTTP_401_FORBIDDEN,
+                detail={"error": "Require field missing", "field": field}
+            )
+        return
+    
+    @staticmethod
+    def check_for_liscense_id(lid, session_user):
+        vehicles = load_json("data/vehicles.json")
+        uvehicles = vehicles.get(session_user["username"], {})
+    
+        if lid in uvehicles:
+                raise HTTPException(
+                status_code=status.HTTP_401_FORBIDDEN,
+                detail={"error": "Vehicle already exists", "data": uvehicles.get(lid)}
+            )
+        return
+    @staticmethod
+    def check_for_liscense_id_not_exists(lid,session_user ):
+        vehicles = load_json("data/vehicles.json")
+        uvehicles = vehicles.get(session_user["username"], {})
+    
+        if lid not in uvehicles:
+                raise HTTPException(
+                status_code=status.HTTP_401_FORBIDDEN,
+                detail={"error": "Vehicle does not exist", "data": uvehicles.get(lid)}
+            )
+        return
 
     #Post
     @staticmethod
-    def CreateVehicle(token : str) :
+    def CreateVehicle(token : str, data : dict) :
         session_user = ValidationService.validate_session_token(token)
-        pass
+        VehicleService.check_for_parameters(data)
+        
+        vehicles = load_json("data/vehicles.json")
+        uvehicles = vehicles.get(session_user["username"], {})
+        
+        lid = data["license_plate"].replace("-", "")
+        ValidationService.check_for_liscense_id(lid,session_user)
+        
+       
+        if not uvehicles:
+            vehicles[session_user["username"]] = {}
+        
+        vehicles[session_user["username"]][lid] = {
+            "licenseplate": data["license_plate"],
+            "name": data["name"],
+            "created_at": datetime.now(),
+            "updated_at": datetime.now()
+        }
+        
+        save_data("data/vehicles.json", vehicles)
+        return {"status": "Success", "vehicle": data}
 
     @staticmethod
-    def ActOnVehicle(token : str) :
+    def ActOnVehicle(token : str, data : dict) :
         session_user = ValidationService.validate_session_token(token)
-        pass
+        ValidationService.check_for_parameters(data)
+        lid = data["license_plate"].replace("-", "")
+        vehicles = load_json("data/vehicles.json")
+        if not "parkinglot" in data :
+            raise HTTPException(
+                status_code=status.HTTP_401_FORBIDDEN,
+                detail={"error": "Require field missing", "field": "parkinglot"}
+            )
+        ValidationService.check_for_liscense_id_not_exists(lid, session_user)
+        return {"status": "Accepted", "vehicle": vehicles[session_user["username"]][lid]}
+ 
 
     #Put 
     @staticmethod
-    def ChangeVehicle(token : str) :
+    def ChangeVehicle(token : str, vid : str, license_plate : str, name : str) :
         session_user = ValidationService.validate_session_token(token)
-        pass
+      
+        if not name:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Required field missing: name"
+            )
+        
+        vehicles = load_vehicle_data()
+        uvehicles = vehicles.get(session_user["username"], {})
+
+        if not uvehicles:
+            vehicles[session_user["username"]] = {}
+        
+ 
+        if vid not in uvehicles:
+            vehicles[session_user["username"]][vid] = {
+                "licenseplate": license_plate,
+                "name": name,
+                "created_at": datetime.now(),
+                "updated_at": datetime.now()
+            }
+        else:
+            
+            vehicles[session_user["username"]][vid]["name"] = name
+            vehicles[session_user["username"]][vid]["updated_at"] = datetime.now()
+        
+        save_data("data/vehicles.json", vehicles)
+        return {"status": "Success", "vehicle": vehicles[session_user["username"]][vid]}
 
     #Delete
     @staticmethod
-    def DeleteVehicle(token : str) :
+    def DeleteVehicle(token : str , vid : str) :
         session_user = ValidationService.validate_session_token(token)
-        pass
+        VehicleService.checkForVehicle(session_user, vid)
+        vehicles = load_vehicle_data()
+        del vehicles[session_user["username"]][vid]
+        save_data("data/vehicles.json", vehicles)
+        return {"status": "Deleted"}
 
     #Get
     @staticmethod
@@ -51,7 +145,8 @@ class VehicleService:
             user = user_name
             UserService.user_exists(user)
             return vehicles.get(user, {})
-        return          
+        return 
+         
     
     @staticmethod
     def get_all_vehicles(token : str): 
