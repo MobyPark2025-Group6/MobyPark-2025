d93a25c2bc064b47d07fa45ec4f816a97024ce31
WarMartijn <1058740@hr.nl>
2025-11-11 09:20:41 +0100

    vehicle_testing test

--- api/Tests/*_test.py
+++ api/Tests/*_test.py
@@ -1,5 +1,5 @@
 import pytest
-
+from unittest.mock import patch
 from models.vehicle_models import Vehicle
 from storage_utils import load_vehicle_data,  save_vehicle_data
 from services.vehicle_service import VehicleService
@@ -7,8 +7,10 @@ from services.user_service import *
 from models.user_models import * 
 import json
 
+
 authorization = None 
 class TestMobyPark:
+
 
     authorization = None 
     def test_authenticate_user(self):
